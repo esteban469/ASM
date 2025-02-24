a356d1a2a678068ae11dc2c08165d0773c369d97
Esteban Padilla <estebanpadilla469@gmail.com>
2025-02-24 11:11:26 -0600

    Inicio nuevo proyecto ASM

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASM
+{
+    public class Program : Token
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lenguaje lexico = new("prueba.cpp"))
+                {
+                    lexico.Programa();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
